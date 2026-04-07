848f636394a60ff144231bb1fe7ad27964dac690
Rocio <rochiserrano1@gmail.com>
2026-04-07 13:17:11 -0300

    Agrego Program00.cs

---
+++ Program00/Program00.cs
@@ -0,0 +1,13 @@
+﻿﻿namespace Programa00;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, mi nombre es Rocio Serrano");
+        Console.WriteLine("Tengo 28 años");
+        Console.WriteLine("Me encuentro cursando la materia Programacion 3 Comision 8");
+        Console.WriteLine("La misma pertenece a la carrera");
+        Console.WriteLine("Tecnicatura Universitaria en Programacion");
+        Console.WriteLine("Dictada en Universidad Tecnologica Nacional");
+    }
+}
