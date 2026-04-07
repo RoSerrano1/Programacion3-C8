02db1abfc2d5ca8407dae1ef95b2b2dc635f88b1
Rocio <rochiserrano1@gmail.com>
2026-04-07 13:18:29 -0300

    Agrego Program01.cs

---
+++ Program01/Program01.cs
@@ -0,0 +1,28 @@
+﻿﻿namespace Program01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido/a, vamos a gestionar tu usuario");
+        Console.WriteLine("Por favor ingresa tu nombre completo");
+        string nombre= Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingresa el usuario deseado:");
+        string usuario= Console.ReadLine();
+
+        Console.WriteLine("Ingresa tu contraseña");
+        string contraseña=Console.ReadLine();
+
+        Console.WriteLine("----- Los datos cargados son: -----");
+        Console.WriteLine("Nombre completo: " + nombre);
+        Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("Usuario: " + usuario);
+        Console.WriteLine("Contraseña: " + contraseña);
+
+        Console.WriteLine("\n Sus datos quedan registrados, presione enter para salir");
+    }
+}
